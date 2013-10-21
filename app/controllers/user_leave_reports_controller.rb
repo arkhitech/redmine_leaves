74fe3e63dd5d196706133c3d83d99e662460237c
Hisham Malik <hisham@arkhitech.com>
2013-10-22 02:20:28 +0500

    adjusted report generation work flow to seamlessly generate query based on parameters selected. Adjusted naming conventions

--- app/controllers/user_leave_reports_controller.rb
+++ app/controllers/user_leave_reports_controller.rb
@@ -2,42 +2,45 @@ class UserLeaveReportsController < ApplicationController
   unloadable
   
   before_filter :require_login
+
+  include UserLeaveReportsHelper
   
   def index
-
     @all_users = User.all
-    @all_leaves = (Setting.plugin_redmine_leaves['leave_types']||"").split(",")
-
+    @all_leaves = plugin_setting('leave_types')
   end
   
   def report
-
-    @reported_user = nil
-    if params[:report].nil?
-      flash.now[:error] = 'Please select user(s) / leave type(s)'
+    @user_leaves = nil
+    if params[:user_leave_report].nil?
+      flash.now[:error] = 'Please provide a report criteria'
     else
-      if params[:report][:selected_users] == nil
-        selected_users = nil
-      else
-        selected_users = User.find(params[:report][:selected_users])
+      where_statements = []
+      where_clause = ['']
+
+      unless params[:user_leave_report][:selected_users].nil?
+        where_statements << 'user_id IN (?)'
+        where_clause << params[:user_leave_report][:selected_users]
       end
 
-      selected_leave_types = params[:report][:selected_leave_types]
-  
-      if(selected_users == nil || selected_leave_types == nil)
-        flash.now[:error] = 'Please select user(s) / leave type(s)'
-      else
-        if params[:entered_date][:date_from].blank? || params[:entered_date][:date_to].blank?
-          @reported_user = UserLeave.
-                where(['user_id IN (?) AND leave_type IN (?)',
-                selected_users, selected_leave_types]).group(:leave_date)
-        else
-          @reported_user = UserLeave.
-                where(['user_id IN (?) AND leave_type IN (?) AND leave_date <= ? AND leave_date >= ?',
-                selected_users, selected_leave_types,
-                params['entered_date']['date_to'].to_date, params['entered_date']['date_from'].to_date]).group(:leave_date)
-        end
+      selected_leave_types = params[:user_leave_report][:selected_leave_types]
+      unless selected_leave_types.nil?
+        where_statements << 'leave_type IN (?)'
+        where_clause << selected_leave_types
       end
+
+      unless params[:user_leave_report][:date_from].blank? 
+        where_statements << 'leave_date >= ?'
+        where_clause << params[:user_leave_report][:date_from]
+      end
+      
+      unless params[:user_leave_report][:date_to].blank?
+        where_statements << 'leave_date <= ?'
+        where_clause << params[:user_leave_report][:date_to]
+      end
+
+      where_clause[0] = where_statements.join(' AND ') 
+      @user_leaves = UserLeave.where(where_clause)
     end
   
   end
--- app/helpers/user_leave_reports_helper.rb
+++ app/helpers/user_leave_reports_helper.rb
@@ -3,12 +3,15 @@ module UserLeaveReportsHelper
   def plugin_setting(setting_name)
     (Setting.plugin_redmine_leaves[setting_name] || '').split(',')
   end
-  
-  def user_options(selected_users)
+
+  def eligible_for_leave_users
     group_ids = Setting.plugin_redmine_leaves['eligible_for_leave_groups']
-    eligible_users = User.active.joins(:groups).
+    User.active.joins(:groups).
       where("#{User.table_name_prefix}groups_users#{User.table_name_suffix}.id" => group_ids)
-    options_from_collection_for_select(eligible_users, :id, :name, selected_users)
+  end
+
+  def user_options(selected_users)
+    options_from_collection_for_select(eligible_for_leave_users, :id, :name, selected_users)
   end
   
   def leave_options(selected_leave_types)
