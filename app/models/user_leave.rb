6b13db16dcb4f34fac6938db4a30364a8af9e23f
Babar Rehman <babar.rehman@arkhitech.com>
2013-11-20 18:09:14 +0500

    Mailer Configured, To send an email on each successful leave creation

--- app/models/user_leave.rb
+++ app/models/user_leave.rb
@@ -1,8 +1,12 @@
 class UserLeave < ActiveRecord::Base
   unloadable
   belongs_to :user
-  
+  after_save :notify_the_absentee
   validates :leave_date, :user_id, :leave_type, presence: true
   validates :leave_date, uniqueness: {scope: :user_id, message: 'has already been marked for the user'}
-
+  
+    def notify_the_absentee
+      LeaveMailer.notify_absentee(self).deliver
+    end
+  private :notify_the_absentee
 end
