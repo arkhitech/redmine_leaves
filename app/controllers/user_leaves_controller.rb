36f53e774f04c1a8f0cf4ca023d2a16062cea684
Abuzar <abuzar.hasan@arkhitech.com>
2013-11-21 16:51:42 +0500

    multiple-leaves task not complete yet. need to pause this task for a while due to another task

--- app/controllers/user_leaves_controller.rb
+++ app/controllers/user_leaves_controller.rb
@@ -6,14 +6,23 @@ class UserLeavesController < ApplicationController
   end
   
   def create    
-    @user_leave = UserLeave.create(params[:user_leave])
+    #    @user_leave = UserLeave.create()
+    selected_users = params['create_user_leave']['selected_users'] || ''
+    selected_group_users = User.active.joins(:groups).
+      where("#{User.table_name_prefix}groups_users#{User.table_name_suffix}.id" => params['create_user_leave']['selected_groups'])
     
-    if @user_leave.errors.empty?
-      flash[:notice] = 'Leave Added!'
-      redirect_to user_leave_reports_path
-    else 
-      render 'new'
+    selected_group_users.each do |user|
+      selected_users << User.find(user).id
     end
+    selected_users = selected_users.uniq
+    puts selected_users
+
+    #    if @user_leave.errors.empty?
+    #      flash[:notice] = 'Leave Added!'
+    #      redirect_to user_leave_reports_path
+    #    else 
+    #      render 'new'
+    #    end
   end
   
   def edit
