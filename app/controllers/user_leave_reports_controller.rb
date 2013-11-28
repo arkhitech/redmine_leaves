feb02864cf664e98d17cdd7532d8ff2c8adbe07f
Babar Rehman <babar.rehman@arkhitech.com>
2013-11-28 12:05:27 +0500

    Commiting before creating hashing the output for devided report view

--- app/controllers/user_leave_reports_controller.rb
+++ app/controllers/user_leave_reports_controller.rb
@@ -6,7 +6,7 @@ class UserLeaveReportsController < ApplicationController
   include UserLeaveReportsHelper
     
   def index
-#    init_group_users
+    #    init_group_users
     @all_users = User.all
     @all_leaves = plugin_setting('leave_types')
   end
@@ -15,10 +15,19 @@ class UserLeaveReportsController < ApplicationController
     @user_leaves = nil
     where_statements = []
     where_clause = ['']
-
-    unless params[:user_leave_report][:selected_users].nil?
-      where_statements << 'user_id IN (?)'
-      where_clause << params[:user_leave_report][:selected_users]
+    selected_groups=params[:user_leave_report][:selected_groups]
+    unless params[:user_leave_report][:selected_users].nil? && selected_groups.nil?
+      selected_groups=params[:user_leave_report][:selected_groups]
+      where_statements << 'user_id IN (?)' 
+      all_users=[]
+      all_users << params[:user_leave_report][:selected_users]
+      unless selected_groups.nil?
+        selected_groups.each do |selected_group|
+          group=Group.where(lastname: selected_group)
+          all_users << group.first.users
+        end
+        where_clause << all_users.flatten.uniq
+      end
     end
 
     selected_leave_types = params[:user_leave_report][:selected_leave_types]
@@ -36,7 +45,7 @@ class UserLeaveReportsController < ApplicationController
       where_statements << 'leave_date <= ?'
       where_clause << params[:user_leave_report][:date_to]
     end
-
+ 
     where_clause[0] = where_statements.join(' AND ') 
     @user_leaves = UserLeave.where(where_clause).order('leave_date desc')
   
