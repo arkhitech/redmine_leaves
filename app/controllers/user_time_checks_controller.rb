b8c8e996f24edab4f4e8da68bd9116bc0ce98fbd
Asim Mushtaq <asim.mushtaq@arkhitech.com>
2013-12-06 11:45:10 +0500

    Adds partial views, corrects the review specefication and time conversion numbers

--- app/controllers/user_time_checks_controller.rb
+++ app/controllers/user_time_checks_controller.rb
@@ -14,10 +14,6 @@ class UserTimeChecksController < ApplicationController
     end
   end
   
-  def home
-    
-  end
-
   def check_out
     checkout_timechecks = UserTimeCheck.where(['user_id = ? AND check_out_time IS NULL', User.current.id])
     
@@ -29,23 +25,18 @@ class UserTimeChecksController < ApplicationController
       @user_time_check = checkout_timechecks.first
       @user_time_check.update_attributes(check_out_time: DateTime.now)
       
-     
       
-      @time_entries= TimeEntry.where(user_id: User.current.id , 
-        created_on: (@user_time_check.check_in_time)..@user_time_check.check_out_time, spent_on: [@user_time_check.check_in_time.to_date,@user_time_check.check_out_time.to_date])
+      @time_entries= TimeEntry.where(user_id: User.current.id , created_on: (@user_time_check.check_in_time)..@user_time_check.check_out_time, spent_on: [@user_time_check.check_in_time.to_date,@user_time_check.check_out_time.to_date])
   
-
+      #@new_time_entry=TimeEntry.new
        logged_in_time= @time_entries.sum(:hours)
          checked_time = @user_time_check.check_out_time - @user_time_check.check_in_time
          
       if logged_in_time<0.9*(checked_time/3600)
          flash.now[:error] = 'Your logged in  time is less than required Percentage. Log your remaining time'
-         @assigned_issues= Issue.where(assigned_to_id: User.current.id).joins(:status).
-           where("#{IssueStatus.table_name}.is_closed" => false)
-      
-         #@assigned_issues= Issue.where("assigned_to_id=? AND  issue_id =?"",  User.current.id)
-       
-      
+         @assigned_issues= Issue.where(assigned_to_id: User.current.id)
+         
+         #@new_time_entries = Array.new(3) { assigned_issue.time_entries.build }
          @new_time_entries = []
          @assigned_issues.each do |assigned_issue|
           #@new_time_entries << TimeEntry.new(:issue_id => assigned_issue.id)
@@ -72,10 +63,7 @@ class UserTimeChecksController < ApplicationController
       @new_time_entries << time_entry_this
     end
     
-         #@assigned_issues= Issue.where(assigned_to_id: User.current.id)
-          @assigned_issues= Issue.where(assigned_to_id: User.current.id).joins(:status).
-           where("#{IssueStatus.table_name}.is_closed" => false)
-         
+         @assigned_issues= Issue.where(assigned_to_id: User.current.id)
          @user_time_check = UserTimeCheck.where(["user_id = ? and check_out_time IS NOT NULL", User.current.id]).limit(1).order('id DESC').first
         # @time_entries= TimeEntry.where(user_id: User.current.id , spent_on: (@user_time_check.check_in_time)..@user_time_check.check_out_time)
         @time_entries= TimeEntry.where(user_id: User.current.id , created_on: (@user_time_check.check_in_time)..@user_time_check.check_out_time+1.hour, spent_on: [@user_time_check.check_in_time.to_date,@user_time_check.check_out_time.to_date])
@@ -92,5 +80,6 @@ class UserTimeChecksController < ApplicationController
     
   end
   
+ 
   
 end
