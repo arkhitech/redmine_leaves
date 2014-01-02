1bebb09b2b19ab2b7860cb747aeb20ef5eed257f
Abuzar Hasan <abuzar.hasan@arkhitech.com>
2014-01-02 16:59:41 +0500

    instructions added for user time checks file format

--- app/controllers/user_time_checks_controller.rb
+++ app/controllers/user_time_checks_controller.rb
@@ -23,10 +23,10 @@ class UserTimeChecksController < ApplicationController
   def import
     begin
       UserTimeCheck.import(params[:file])
+      redirect_to user_time_checks_path, notice: "User Time Checks Imported" if params[:file]
     rescue StandardError => e
-      redirect_to user_time_checks_path, :flash => { :error => 'Invalid File!' }
-    end  
-    redirect_to user_time_checks_path, notice: "User Time Checks Imported" if params[:file]
+      redirect_to user_time_checks_path, :flash => { :error => 'Invalid File Format!' }
+    end    
   end
   
   def check_in
