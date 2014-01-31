988d618a3cdaf21b4e63b4936dbef667fb5d4f58
Abuzar Hasan <abuzar.hasan@arkhitech.com>
2014-01-31 13:16:29 +0500

    restricts marking own leave while adding group leave if user is not allowed to mark own leave

--- app/helpers/user_leaves_helper.rb
+++ app/helpers/user_leaves_helper.rb
@@ -4,14 +4,17 @@ module UserLeavesHelper
     (Setting.plugin_redmine_leaves[setting_name] || '').split(',').delete_if { |index| index.blank? }
   end
   
+  def mark_leave_options(mark_leave)
+    User.active.joins(:groups).
+      where("#{User.table_name_prefix}groups_users#{User.table_name_suffix}.id" => mark_leave)
+  end
+  
   def add_user_options(selected_user)    
     mark_leave_groups = Setting.plugin_redmine_leaves['mark_leaves']
-    mark_leave_users = User.active.joins(:groups).
-      where("#{User.table_name_prefix}groups_users#{User.table_name_suffix}.id" => mark_leave_groups)
+    mark_leave_users = mark_leave_options(mark_leave_groups)
     
     mark_own_leave_groups = Setting.plugin_redmine_leaves['mark_own_leave']
-    mark_own_leave_users = User.active.joins(:groups).
-      where("#{User.table_name_prefix}groups_users#{User.table_name_suffix}.id" => mark_own_leave_groups)
+    mark_own_leave_users = mark_leave_options(mark_own_leave_groups)
     
     if(mark_leave_users.include?(User.current) && mark_own_leave_users.include?(User.current))
       # if current user have both permissions
@@ -32,12 +35,10 @@ module UserLeavesHelper
   
   def add_group_options(selected_group)
     mark_leave_groups = Setting.plugin_redmine_leaves['mark_leaves']
-    mark_leave_users = User.active.joins(:groups).
-      where("#{User.table_name_prefix}groups_users#{User.table_name_suffix}.id" => mark_leave_groups)
+    mark_leave_users = mark_leave_options(mark_leave_groups)
     
     mark_own_leave_groups = Setting.plugin_redmine_leaves['mark_own_leave']
-    mark_own_leave_users = User.active.joins(:groups).
-      where("#{User.table_name_prefix}groups_users#{User.table_name_suffix}.id" => mark_own_leave_groups)    
+    mark_own_leave_users = mark_leave_options(mark_own_leave_groups)
     
     unless(!mark_leave_users.include?(User.current) && mark_own_leave_users.include?(User.current))      
       # if current user have permission to mark own leaves      
@@ -52,6 +53,18 @@ module UserLeavesHelper
   def add_leave_options(selected_leave_type)
     all_leave_types = plugin_setting('leave_types')
     options_for_select(all_leave_types, selected_leave_type)
+  end
+  
+  def check_selected_users(selected_users)
+    mark_leave_groups = Setting.plugin_redmine_leaves['mark_leaves']
+    mark_leave_users = mark_leave_options(mark_leave_groups)
+    
+    mark_own_leave_groups = Setting.plugin_redmine_leaves['mark_own_leave']
+    mark_own_leave_users = mark_leave_options(mark_own_leave_groups)
+    if(mark_leave_users.include?(User.current) && !mark_own_leave_users.include?(User.current))      
+      selected_users = selected_users - [User.current.id.to_s]
+    end
+    selected_users
   end
   
 end
