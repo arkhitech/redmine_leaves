2ba0b05900c976d961f4e3a2aeebef2de752a822
Rashid Mehmood Khalid <rashid.mehmood@arkhitech.com>
2014-07-10 17:46:16 +0500

    warns user when leave is added/updated on an already non-working day

--- assets/javascripts/redmine_leaves.js
+++ assets/javascripts/redmine_leaves.js
@@ -1,56 +1,49 @@
 $(document).ready(function() {
 
-$('#new_user_leave').submit(function() {
-    
-    var dateFrom = new Date($('#create_user_leave_selected_date_from').val());
-    var dateTo = new Date($('#create_user_leave_selected_date_to').val());
-    var data_working_days = $('#new_user_leave').attr('data-working-days').split(',');
-    var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var countNonWorkingDays = 0;
-    //$('#settings_working_days')
-    
-    for(var dayCount = 0 ; dayCount <= ((dateTo.getTime() - dateFrom.getTime())/86400000) ; dayCount++){
-         
-    var iterating_day = new Date(Date.parse(dateFrom)+(dayCount * 86400000));
-    
-        if(data_working_days.indexOf(weekDays[iterating_day.getDay()]) < 0){
-            countNonWorkingDays++;
+        var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    $('#new_user_leave').submit(function(event) {
+
+        var dateFrom = new Date($('#create_user_leave_selected_date_from').val());
+        var dateTo = new Date($('#create_user_leave_selected_date_to').val());
+        var data_working_days = $('#new_user_leave').attr('data-working-days').split(',');
+        var countNonWorkingDays = 0;
+        var allOk = true;
+
+        for (var dayCount = 0; dayCount <= ((dateTo.getTime() - dateFrom.getTime()) / 86400000); dayCount++) {
+
+            var iterating_day = new Date(Date.parse(dateFrom) + (dayCount * 86400000));
+
+            if (data_working_days.indexOf(weekDays[iterating_day.getDay()]) < 0) {
+                countNonWorkingDays++;
+            }
         }
-    }
-      if(countNonWorkingDays > 0){
-    confirm('This selection includes ' + countNonWorkingDays + ' already non-working days!' );
-}}
-    );
-
-
-//var flag_weekend = 0;
-//var iterating_day = Date(params['create_user_leave']['selected_date_from']);
-//    
-//    while (iterating_day <= Date(params['create_user_leave']['selected_date_to'])){
-//      if (Setting.plugin_redmine_leaves['working_days'].indexOf(iterating_day.getDay()) < 0){
-//        flag_weekend += 1;
-//	}
-//        iterating_day += 1;
-//    }
-//    
-//    if (flag_weekend > 0){
-//      confirm("There are" + flag_weekend + "non-working days in your leave dates.");
-//    }
-    
         
-    //new Date(Date.parse(a)+86400000)
+        if (countNonWorkingDays > 0) { 
+            allOk = confirm('This selection includes ' + countNonWorkingDays + ' already non-working days!');
+        }
+        if(allOk) {
+            $('#new_user_leave').unbind('submit');
+            $('#new_user_leave').submit();            
+        }
+        return allOk;
+    }); 
     
-    //dateFrom = Date(dateFrom.getTime() + (5 * 24 * 60 * 60 * 1000));  
-       
-    // (dateFrom.setDate(dateFrom.getDay() + 1))    
-    //Date(dateFrom.setDate(dateFrom.getTime() + (1 * 24 * 60 * 60 * 1000)))
-   
-   
-    
-    
-    //TODO get value of start date and end date
-    //see if they non-working days are included
-    //if not, then let user submit
-    //if included, then warn user
+    $('.edit_user_leave').submit(function(event) {
+        
+        var dateObj = new Date(Number($('#user_leave_leave_date_1i').val()), Number($('#user_leave_leave_date_2i').val())-1, Number($('#user_leave_leave_date_3i').val()));
+        var data_working_days = $('.edit_user_leave').attr('data-working-days').split(',');
+        var allOk = true;
+        
+        console.log(dateObj);
+        
+        if (data_working_days.indexOf(weekDays[dateObj.getDay()]) < 0) {   
+            allOk = confirm('This selection is a non-working day!');
+        }
+        if(allOk) {
+            $('.edit_user_leave').unbind('submit');
+            $('.edit_user_leave').submit();            
+        }
+        return allOk;
+    }); 
     
 });
