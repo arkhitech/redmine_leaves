a60ec13d8fee0e99adc21581537e86daa57d5c46
Arbaz Sajid <arbaz.sajid@arkhitech.com>
2015-12-01 10:45:01 +0500

    final commit of leaves and redmine

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ resources :user_time_checks, :only => [:index, :update, :edit] do
   collection { post :import }
 end
 
-match '/create_time_entries' => 'user_time_checks#create_time_entries'
+match '/create_time_entries' => 'user_time_checks#create_time_entries', via: :get
 
 #get "user_time_checks/home"
 get "user_time_checks/check_in"
@@ -25,9 +25,9 @@ post "user_time_checks/user_time_activity_report_monthly"
 
 
 #match '/home', to: 'user_time_checks#home', via: 'get'
-match '/check_in' => 'user_time_checks#check_in'
-match '/check_out' => 'user_time_checks#check_out'
-match '/checkout_timelog_success' => 'user_time_checks#checkout_timelog_success'
-match '/user_leave_report' => 'user_leave_reports#report'
-match '/user_leave_analytics' => 'user_leave_analytics#report'
+match '/check_in' => 'user_time_checks#check_in', via: :get
+match '/check_out' => 'user_time_checks#check_out', via: :get
+match '/checkout_timelog_success' => 'user_time_checks#checkout_timelog_success', via: :get
+match '/user_leave_report' => 'user_leave_reports#report', via: [:get, :post]
+match '/user_leave_analytics' => 'user_leave_analytics#report', via: :get
 
